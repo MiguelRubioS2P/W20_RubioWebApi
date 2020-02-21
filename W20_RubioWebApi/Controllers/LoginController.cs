7d4c809054c1023e431cf604298de9a1c4fb55dd
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2020-02-21 18:51:30 +0100

    entrega

---
+++ W20_RubioWebApi/Controllers/LoginController.cs
@@ -0,0 +1,100 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using W20_RubioWebApi.Models;
+
+namespace W20_RubioWebApi.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Login")]
+    public class LoginController : ApiController
+    {
+
+        // POST api/Login/RegisterLogin
+        [HttpPost]
+        [Route("RegisterLogin")]
+        public IHttpActionResult RegisterLogin(LoginModel login)
+        {
+            using (IDbConnection cnn = new ApplicationDbContext().Database.Connection)
+            {
+                // insertamos el login por defecto 1 para indicar que esta login cuando recibimos el login ya activo
+                string sql = "INSERT INTO dbo.Logins(Id, Name, Login) " +
+                    $"VALUES('{login.Id}','{login.Name}', 1)";
+                try
+                {
+                    cnn.Execute(sql);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Error inserting login in database: " + ex.Message);
+                }
+                finally
+                {
+                    cnn.Close();
+                }
+
+                return Ok();
+            }
+        }
+
+        [HttpPost]
+        [Route("CerrarLogin")]
+        public IHttpActionResult CerrarLogin(LoginModel login)
+        {
+            using(IDbConnection cnn = new ApplicationDbContext().Database.Connection)
+            {
+                string sql = "DELETE FROM dbo.Logins " +
+                    $"where id = {login.Id}";
+
+                try
+                {
+                    cnn.Execute(sql);
+
+                }catch(Exception ex)
+                {
+                    return BadRequest("Error cerrando login in database: " + ex.Message);
+                }
+                finally
+                {
+                    cnn.Close();
+                }
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("UsuariosEnLinea")]
+        public List<string> UsuariosEnLinea()
+        {
+            List<string> resultado = new List<string>();
+
+            using (IDbConnection cnn = new ApplicationDbContext().Database.Connection)
+            {
+                string sql = "select * from dbo.Logins where Login = 1";
+
+                try
+                {
+                    resultado = cnn.Query<string>(sql).ToList();
+                }
+                catch(Exception ex)
+                {
+                    throw new Exception("Error para ver la lista: " + ex.Message);
+                }
+                finally
+                {
+                    cnn.Close();
+                }
+
+            }
+
+                return resultado;
+        }
+
+    }
+}
---
+++ W20_RubioWebApi/Models/LoginModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace W20_RubioWebApi.Models
+{
+    public class LoginModel
+    {
+        private string _id;
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _login;
+        public int Login
+        {
+            get { return _login; }
+            set { _login = value; }
+        }
+    }
+}
